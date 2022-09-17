37c4fd8296afa9b0492c5e6b2d263122023a831e
Vipul Tripathi <vipul.trp39@gmail.com>
2022-09-17 20:34:43 +0530

    first prog

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter DoB (dd/mm/yyyy)");
+        String dob = sc.nextLine();
+        String[] arr = dob.split("/");
+        int d = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+        int y = Integer.parseInt(arr[2]);
+        var d1 = LocalDate.of(y,m,d);
+        var d2 = LocalDate.now();
+
+        var p = Period.between(d1,d2);
+
+        System.out.println(p.getYears()+" years"+p.getMonths()+
+                " Months"+ p.getDays()+" Days");
+    }
+}
